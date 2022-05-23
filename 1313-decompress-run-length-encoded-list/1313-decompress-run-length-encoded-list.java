cce28ccef4260fd4a2a18247836313dabd90fac0
Mohamed Abdullahi <77897855+Moha-dotcom@users.noreply.github.com>
2022-05-23 09:45:41 -0500

    Time: 3 ms (58.93%), Space: 47.9 MB (56.40%) - LeetHub

--- 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.java
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.java
@@ -1,9 +1,10 @@
 class Solution {
     public int[] decompressRLElist(int[] nums) {
+        // To create new array we need to find the size
         int size=0;
         for(int i=0;i<nums.length;i++){
             if(i%2==0){
-                size=size+nums[i];
+                size=size+nums[i]; // We get the any even number an add it to the size
             }
         }
         
