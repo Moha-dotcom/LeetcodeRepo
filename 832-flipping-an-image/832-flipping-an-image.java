bca7fd02a1da6787c6dce2c507cfd7a6271e0bd0
Mohamed Abdullahi <77897855+Moha-dotcom@users.noreply.github.com>
2022-05-23 12:02:55 -0500

    Time: 0 ms (100.00%), Space: 44.8 MB (51.75%) - LeetHub

--- 832-flipping-an-image/832-flipping-an-image.java
+++ 832-flipping-an-image/832-flipping-an-image.java
@@ -11,9 +11,9 @@ class Solution {
         int start = 0;
         int end = arr.length-1;
         while(start <= end){
-            int temp = 1-arr[start];
-            arr[start] = 1-arr[end];
-             arr[end]=temp;
+            int temp=1-arr[start]; // {1, 1, 0} = 0;
+            arr[start]=1-arr[end]; // 1- 0; 1;
+            arr[end] = temp;
             start++;
             end--;
             
