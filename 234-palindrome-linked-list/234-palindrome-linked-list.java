516a138301fe631feddce87baaa7381cbdbc6292
Mohamed Abdullahi <77897855+Moha-dotcom@users.noreply.github.com>
2022-05-26 12:32:22 -0500

    Time: 5 ms (85.24%), Space: 98 MB (41.45%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -11,28 +11,36 @@
 class Solution {
     public boolean isPalindrome(ListNode head) {
      
-        ListNode slow = head;
-        boolean ispalin = true;
-        Stack<Integer> stack = new Stack<Integer>();
- 
-        while (slow != null) {
-            stack.push(slow.val);
-            slow = slow.next;
+        int length = 0;
+        ListNode temp = head;
+        
+        //calculate length
+        while(temp!=null){
+            temp = temp.next;
+            length++;
         }
- 
-        while (head != null) {
- 
-            int i = stack.pop();
-            if (head.val == i) {
-                ispalin = true;
-            }
-            else {
-                ispalin = false;
-                break;
-            }
-            head = head.next;
+        //initialise array to store first half of list
+        int[] arr = new int[length/2];
+        
+        int i = 0;
+        temp = head;
+        while(i < length/2){
+            arr[i] = temp.val;
+            temp = temp.next;
+            i++;
         }
-        return ispalin;
+        
+        //if odd number of elements, no need to compare the middle element
+        if(length%2 != 0) temp=temp.next;
+        
+        i--;
+        //compare the second half of list element by element
+        while(temp!= null){
+            if(arr[i] != temp.val) return false;
+            temp = temp.next;
+            i--;
+        }
+        return true;
     }
     
 }
