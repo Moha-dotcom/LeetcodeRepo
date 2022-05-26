3c4b519d279a2f5351df155c22e8a368c5b5dcf1
Mohamed Abdullahi <77897855+Moha-dotcom@users.noreply.github.com>
2022-05-26 12:38:43 -0500

    Time: 40 ms (10.46%), Space: 110.5 MB (12.69%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -11,64 +11,38 @@
 class Solution {
     public boolean isPalindrome(ListNode head) {
      
-
- int length = 0;
-        ListNode temp = head;
+        ListNode curr = head;
+        boolean isTrueOrFalse = true;
+        Stack<Integer> stack = new Stack<>();
         
-        //calculate length
-        while(temp!=null){
-            temp = temp.next;
-            length++;
-        }
-        //initialise array to store first half of list
-        int[] arr = new int[length/2];
         
-        int i = 0;
-        temp = head;
-        while(i < length/2){
-            arr[i] = temp.val;
-            temp = temp.next;
-            i++;
+        
+        while(curr != null){
+            stack.push(curr.val);
+            curr = curr.next;
         }
         
-        //if odd number of elements, no need to compare the middle element
-        if(length%2 != 0) temp=temp.next;
-        
-        i--;
-        //compare the second half of list element by element
-        while(temp!= null){
-            if(arr[i] != temp.val) return false;
-            temp = temp.next;
-            i--;
+        while(head != null){
+            int i = stack.pop();
+            
+            if(head.val == i){
+                isTrueOrFalse = true;
+               
+            }else{
+                isTrueOrFalse = false;
+                break;
+            }
+            
+            head = head.next;
+            
         }
-        return true;
+        
+        return isTrueOrFalse;
+        
+        
     }
-    
 }
 
 
 
-//         ListNode slow = head;
-//         boolean ispalin = true;
-//         Stack<Integer> stack = new Stack<Integer>();
- 
-//         while (slow != null) {
-//             stack.push(slow.val);
-//             slow = slow.next;
-//         }
- 
-//         while (head != null) {
- 
-//             int i = stack.pop();
-//             if (head.val == i) {
-//                 ispalin = true;
-//             }
-//             else {
-//                 ispalin = false;
-//                 break;
-//             }
-//             head = head.next;
-//         }
-//         return ispalin;
-//     }
     
