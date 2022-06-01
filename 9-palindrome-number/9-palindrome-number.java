50cbd3aa8e1e1ed3a56279ffbdebb6948695ba07
Mohamed Abdullahi <77897855+Moha-dotcom@users.noreply.github.com>
2022-05-31 23:30:42 -0500

    Time: 9 ms (94.32%), Space: 41.7 MB (88.20%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+       
+        
+        if(x >= 0){
+            int sum = 0, n = x;
+        while(x != 0){
+            int a = x % 10;
+            sum = sum * 10 + a;
+            x /= 10;
+        }
+        
+        boolean answer = sum == n ? true : false;
+       
+        
+        return  answer;
+            
+        }else{
+            return false;
+        }
+        
+    }
+}
