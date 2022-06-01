75d8dd49c2d2622836630ad8e9099f761fcd1d58
Mohamed Abdullahi <77897855+Moha-dotcom@users.noreply.github.com>
2022-05-31 23:33:31 -0500

    Time: 13 ms (57.08%), Space: 45.3 MB (24.74%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -4,16 +4,16 @@ class Solution {
         
         if(x >= 0){
             int sum = 0, n = x;
-        while(x != 0){
-            int a = x % 10;
-            sum = sum * 10 + a;
-            x /= 10;
-        }
+            while(x != 0){
+                int a = x % 10;
+                sum = sum * 10 + a;
+                x /= 10;
+            }
         
-        boolean answer = sum == n ? true : false;
+             boolean answer = sum == n ? true : false;
        
         
-        return  answer;
+            return  answer;
             
         }else{
             return false;
